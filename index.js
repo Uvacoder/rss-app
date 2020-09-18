f192b4ca6945846b9ab40c591e1ef07e299f3d19
Chris Bongers <chrisbongers@gmail.com>
2020-09-18 18:33:07 +0200

    Added read write function

--- index.js
+++ index.js
@@ -1,39 +1,20 @@
 let Parser = require('rss-parser');
 let parser = new Parser();
-var FB = require('fb');
-var Twitter = require('twitter');
+const fs = require('fs');
+
+let rawdata = fs.readFileSync('site.json');
+let siteData = JSON.parse(rawdata);
 
 (async () => {
-
-    let feed = await parser.parseURL('https://daily-dev-tips.com/feed.xml');
-
-    console.log(feed.title);
-
+    let feed = await parser.parseURL('https://daily-dev-tips.com/sitemap.xml');
     feed.items.forEach(item => {
-        console.log(item.title);
+        let url = item.id;
+        if (!siteData.url) {
+            // Do magic posting stuff
+            siteData[url] = {
+                'published': true
+            };
+        }
     });
-
+    fs.writeFileSync('site.json', JSON.stringify(siteData));
 })();
-
-FB.setAccessToken('ACCESS_TOKEN');
-FB.api(
-    '/DailyDevTipsBlog/feed',
-    'POST',
-    { "message": "Testing with api" },
-    function (response) {
-        console.log(response);
-    }
-);
-
-var client = new Twitter({
-    consumer_key: '{CONSUMER_KEY}',
-    consumer_secret: '{CONSUMER_SECRET}',
-    access_token_key: '{ACCESS_TOKEN_KEY}',
-    access_token_secret: '{ACCESS_TOKEN_SECRET}'
-});
-
-client.post('statuses/update', { status: 'I Love Twitter' }, function (error, tweet, response) {
-    if (error) throw error;
-    console.log(tweet);  // Tweet body.
-    console.log(response);  // Raw response object.
-});
